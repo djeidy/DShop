e0baad96dcb30a2a7e13676f179f6f1456da4c45
Mahamadou Djeidy Ba <mdjeidyba@atlassian.com>
2020-11-07 17:58:03 -0800

    Add router and routes

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProductScreen = (props) => {
+  return (
+    <div>
+      Product
+    </div>
+  );
+};
+
+ProductScreen.propTypes = {
+
+};
+
+export default ProductScreen;
