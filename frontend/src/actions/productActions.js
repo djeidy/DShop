b5c1e48b84c2bde94d87e71694d906b880f3d33b
Mahamadou Djeidy Ba <mdjeidyba@atlassian.com>
2020-11-08 20:32:26 -0800

    add product list constants, actions and reducers

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,26 @@
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS
+} from "../constants/productConstants";
+import axios from "axios";
+
+export const listProducts = () => async dispatch => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+
+    const { data } = await axios.get("/api/products");
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data
+    });
+  } catch (e) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        e.response && e.response.data.message
+          ? e.response.data.message
+          : e.message
+    });
+  }
+};
